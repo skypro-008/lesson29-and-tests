02395a4a53e9a42b6b9f6f3fbdfe779368e09785
lisvv <81390205+lisvv@users.noreply.github.com>
2022-02-17 10:18:42 +0300

    part2

--- my_project_part_2/router/urls.py
+++ my_project_part_2/router/urls.py
@@ -1,11 +1,5 @@
-from rest_framework import routers
-from django.urls import path, include
-
-from router import views
-
-simple_router = routers.SimpleRouter()
-simple_router.register('router_stores', views.StoreViewSet)
+# TODO опишите здесь маршрутизацию с помощью класса SimpleRouter
 
 urlpatterns = [
-   path('', include(simple_router.urls))
+
 ]
